d25b4c69b650175ac619ef4e245eec474f78e1f8
vlad.tkach <vlad.tkach@asmbrain.com>
2019-06-20 18:58:52 +0300

    add redux to app , create post actions and reducers, create store

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Provider store={store}>
+          <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
 
-      </header>
-      <Posts />
-      <Postform/>
-    </div>
+              </header>
+              <Posts />
+              <Postform/>
+          </div>
+      </Provider>
+
   );
 }
 
